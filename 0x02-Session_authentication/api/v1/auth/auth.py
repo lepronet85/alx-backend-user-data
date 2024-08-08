d17214a18ee2e2f8f149491aa22f3c89e4dd50bf
lepronet <lepronet85@gmail.com>
2024-08-09 00:47:35 +0100

    Update auth file

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,42 +1,65 @@
 #!/usr/bin/env python3
-"""
-Manage API authentication system
+""" Module of Authentication
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
-class Auth():
-    """
-    Manage API authentication methods
-    """
+class Auth:
+    """ Class to manage the API authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Return boolean """
-        if path is None or excluded_paths is None or not len(excluded_paths):
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        if path[-1] != '/':
-            path += '/'
-        if excluded_paths[-1] != '/':
-            excluded_paths += '/'
+        l_path = len(path)
+        if l_path == 0:
+            return True
 
-        astericks = [stars[:-1]
-                     for stars in excluded_paths if stars[-1] == '*']
+        slash_path = True if path[l_path - 1] == '/' else False
 
-        for stars in astericks:
-            if path.startswith(stars):
-                return False
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
 
-        if path in excluded_paths:
-            return False
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Request Flask object """
-        if request is None or 'Authorization' not in request.headers:
+        """ Method that handles authorization header """
+        if request is None:
             return None
-        return request.headers.get('Authorization')
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Flask request object """
+        """ Validates current user """
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+
+        if request is None:
+            return None
+
+        SESSION_NAME = getenv("SESSION_NAME")
+
+        if SESSION_NAME is None:
+            return None
+
+        session_id = request.cookies.get(SESSION_NAME)
+
+        return session_id
